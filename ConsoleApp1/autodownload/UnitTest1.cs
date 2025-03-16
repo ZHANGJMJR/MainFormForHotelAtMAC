8c5bde46e40a935b4cce3ac436c226a7c83a54f2
zhangjmjr <zhangjmjr@gmail.com>
2025-03-16 17:09:50 +0800

    playwright zh-CN support

--- ConsoleApp1/autodownload/UnitTest1.cs
+++ ConsoleApp1/autodownload/UnitTest1.cs
@@ -21,11 +21,23 @@ public class Tests
         using var playwright = await Playwright.CreateAsync();
         await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions 
             { Headless = false,
-                Args = new List<string> { "--start-maximized" } } );
+                Args = new List<string> { "--start-maximized","--lang=zh-CN" } } );
         
         var context = await browser.NewContextAsync(new BrowserNewContextOptions
         {
-            ViewportSize = ViewportSize.NoViewport
+            ViewportSize = ViewportSize.NoViewport,
+            Permissions = new[] { "geolocation" },
+            Locale = "zh-CN",
+            ExtraHTTPHeaders = new Dictionary<string, string>
+            {
+                ["Accept-Language"] = "zh-CN,cn;q=0.9"
+            },
+            Geolocation = new Geolocation
+            {
+                Longitude = 116.4f,
+                Latitude = 39.9024f,
+                Accuracy = 100,
+            },
         });
 
         var page = await context.NewPageAsync();
@@ -39,7 +51,7 @@ public class Tests
         await page.GetByPlaceholder("请输入登录密码").FillAsync(password);
         Task.Delay(2000).Wait();
         //await page.GetByLabel("登 录").ClickAsync();
-        await page.ClickAsync("text=Login");
+        await page.ClickAsync("text=登 录");
         // await page.FillAsync("#UserName","admin");
         // await page.FillAsync("#Password","password");
         Task.Delay(1000).Wait();
